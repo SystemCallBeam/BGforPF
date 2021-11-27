14421159cd3b11e98771d12e23f17cb9518da753
SystemCallBeam <90573740+SystemCallBeam@users.noreply.github.com>
2021-11-27 19:57:59 +0700

    Update bgforport.cpp

--- bgforport.cpp
+++ bgforport.cpp
@@ -3,27 +3,51 @@ using namespace std;
 #define PO(a) (a)*(a)
 //#define n 61
 
-int main(){
-    int n; cin >> n;
-    int M = n/2, B = 1, N = B*n;
-    int h = 0, k = 0;
+void makePicture(int N, int d, char C, int H[], int K[], int R[]){
+    
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < N; j++)
         {
             //printf("(%d, %d) ", i, j);
-            if (abs(i-M) + abs(j-M) <= M-2)
+            int tic = 0;
+            for (int k = 0; k < d; k++)
             {
-                cout << "# ";
+                if (tic) continue;
+                if (abs(i-H[k]) + abs(j-K[k]) <= R[k])
+                {
+                    cout << C << " ";
+                    tic = 1;
+                }
             }
-            else
-            {
-                cout << "~ ";
-            }//*/
+            if(!tic)
+                cout << "~ ";//*/
         }
         cout << "\n"; //\n
     }
+}
+
+void inputData(int d, int H[], int K[], int R[]){
+    for (int i = 0; i < d; i++)
+    {
+        cout << "\ncoordinates (h) (k) (r) (" << i+1 << ") : ";
+        cin >> H[i] >> K[i] >> R[i];
+    }
     
+}
+
+int main(){
+    char C = '#';
+    int n = 61, d = 1;
+    cout << "canva size : "; cin >> n;
+    cout << "\ndimond count : "; cin >> d;
+    cout << "\nstyle : "; cin >> C;
+    int M = n/2, B = 1, N = B*n;
+    int H[d] = {}, K[d] = {}, R[d] = {};
+
+    inputData(d, H, K, R);
+    makePicture(N, d, C, H, K, R);
+
     return 0;
 }
 
